efa764cb4dba07e93a120cf62b4d60eab5e4566d
ryanbs183 <ryan.sullivan.183@gmail.com>
2020-10-28 23:33:22 -0400

    improved threading library

--- thread/threads.c
+++ thread/threads.c
@@ -1,11 +1,3 @@
-#include <pthread.h>
-#include <setjmp.h>
-#include <signal.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "ec440threads.h"
 #include "threads.h"
 
 /* 
@@ -15,32 +7,33 @@
 
 /* global variables */
 nthread *currThread = NULL;		// keeps track of currently executing thread
-int thread_id = 0;
-int numThreads = 0;
+int thread_id = 0;				// current thread id
+int numThreads = 0;				// keeps track of the number of currently active threads
 
-/* function declarations */
-
+/* create a new thread and add it to the queue */
 int pthread_create(pthread_t *thread,const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg){
+	lock();
 	if (numThreads == 0) {
         init();
     } else if (numThreads >= MAXTHREAD) {
         printf("Too many threads, try again later!!\n");
-		return -1;
+		return 1;
     }
 	// create newThread
     nthread *newThread = malloc(sizeof(nthread));
-    thread = (pthread_t *)newThread;
 
 	// allocate thread stack memory
     newThread -> stack_ptr = malloc(STACKMEM);
 
-	// set spot in memory for pthread_exit
-    unsigned long int *exit = (unsigned long int *) newThread -> stack_ptr + (STACKMEM/sizeof(unsigned long int) - 1);	// point to top of stack
-    *exit = (unsigned long int)pthread_exit;	// place pthread_exit at the top of the stack
+	// set spot in memory for pthread_exit_wrapper
+    unsigned long int *exit = (unsigned long int *)(newThread -> stack_ptr + (STACKMEM/sizeof(unsigned long int) - 1));	// point to top of stack
+    *exit = (unsigned long int)pthread_exit_wrapper;	// place pthread_exit_wrapper at the top of the stack
 
 	// initialize newThread
-    newThread -> state = 0;
+    newThread -> state = READY;
     newThread -> id = (pthread_t)thread_id;
+	newThread -> ret_val = NULL;
+	newThread -> tgt_thread = (pthread_t)-1;
     *thread = newThread -> id; 
     newThread -> last = currThread -> last;
     newThread -> next = currThread;
@@ -53,26 +46,76 @@ int pthread_create(pthread_t *thread,const pthread_attr_t *attr, void *(*start_r
 
 	// store current state
     setjmp(newThread -> buf);
-	
+
 	// place start_thunk on the PC to run after longjmp
-    newThread -> buf[0].__jmpbuf[JB_PC] = ptr_mangle((unsigned long int)start_thunk);
+	newThread -> buf[0].__jmpbuf[JB_PC] = ptr_mangle((unsigned long int)start_thunk);
 	// set the exit function as the return stack ptr
-    newThread -> buf[0].__jmpbuf[JB_RSP] = ptr_mangle((unsigned long int)exit);
+	newThread -> buf[0].__jmpbuf[JB_RSP] = ptr_mangle((unsigned long int)exit);
 
 	// increment numThreads and thread_id
     numThreads++;
     thread_id++;
+	unlock();
     return 0;
+}
+
+int pthread_join(pthread_t thread, void **value_ptr){
+	// check for target thread and block current thread
+	nthread* target = thread_exists(currThread, thread);
+	if(target == NULL || thread == currThread -> id){
+		return 1;
+	}
+	lock();
+	currThread -> state = BLOCKED;
+	currThread -> tgt_thread = thread;
+
+	if(target -> state != EXIT){
+		unlock();
+
+		schedule();
+		unlock();
+
+		lock();
+	} else{
+		currThread -> state = READY;
+	}
+
+	if(value_ptr != NULL){
+		nthread* step = currThread -> next;
+		nthread* start = currThread;
+		while(step != start){
+			if(step -> id == thread){
+				*value_ptr = step -> ret_val;
+				break;
+			}
+			step = step -> next;
+		}
+	}
+	unlock();
+	return 0;
+}
+
+/* check to see if thread exists and return its state */
+nthread* thread_exists(nthread* start, pthread_t thread){
+	nthread *step = start -> next;
+	while(step != start){
+		if(step -> id == thread){
+			return step;
+		}
+		step = step -> next;
+	}
+	return NULL;
 }
 
 /* exit current thread and free stack */
 void pthread_exit(void *value_ptr){
-	currThread -> state = 2;
-	currThread -> last -> next = currThread -> next;
-	currThread -> next -> last =  currThread -> last;
-	free(currThread -> stack_ptr);
+	lock();
+	currThread -> state = EXIT;
+	currThread -> ret_val = value_ptr;
 	numThreads--;
+	unlock();
 	schedule();
+	unlock();
 	__builtin_unreachable();
 }
 
@@ -91,8 +134,10 @@ int init(){
 	initThread -> id = (pthread_t)thread_id;
 	initThread -> last = initThread;
 	initThread -> next = initThread;
-	initThread -> stack_ptr = NULL;
-	initThread -> state = 0;
+	initThread -> stack_ptr = malloc(STACKMEM);
+	initThread -> ret_val = NULL;
+	initThread -> state = READY;
+	initThread -> tgt_thread = (pthread_t)-1;
 	numThreads++;
 	thread_id++;
 	setjmp(initThread -> buf);
@@ -102,24 +147,62 @@ int init(){
 
 	currThread = initThread;
 
-	return 1;
+	return 0;
 }
 
 /* pause current thread and jump to next thread */
 void schedule(){
-	/* check to see if uninitialized */
+	// check to see if uninitialized
 	if(currThread == NULL){
 		return;
 	}
+	lock();
+	// save state of current thread 
 	if(setjmp(currThread -> buf) == 0){
-		/* check to make sure schedule is not called after exit */
-		if(currThread -> state != 2){
-			currThread -> state = 3;			// set thread to PAUSE
+		// check to make sure schedule is not called after exit or block
+		if(currThread -> state != EXIT && currThread -> state != BLOCKED && currThread -> state != PAUSED){
+			currThread -> state = READY;	// set thread to READY
 		}
 		currThread = currThread -> next;	// move to next thread in line
-		currThread -> state = 1;			// set new thread to RUNNING
+
+		// keep stepping until thread is no longer blocked or exited
+		while(currThread -> state == BLOCKED|| currThread -> state == EXIT || currThread -> state == PAUSED){
+			if(currThread -> state == BLOCKED){
+				nthread *tgt = thread_exists(currThread, currThread -> tgt_thread);
+				if(tgt != NULL && tgt -> state == EXIT){
+					break;
+				}
+			}
+			currThread = currThread -> next;
+		}
+		currThread -> state = RUNNING;		// set new thread to RUNNING
 		longjmp(currThread -> buf, 1); 		// jump to new thread execution
 	}
+}
+
+/* wrapper function for pthread_exit to allow for saving of return value of thread */
+void pthread_exit_wrapper() {
+	unsigned long int res;
+	asm("movq %%rax, %0\n":"=r"(res));
+	pthread_exit((void *) res);
+}
+
+/* disable interactivity of calling thread */
+void lock(){
+	sigset_t blocked_sig;
+	sigemptyset(&blocked_sig);
+	sigaddset(&blocked_sig, SIGALRM);
+
+	sigprocmask(SIG_BLOCK, &blocked_sig, NULL);
+}
+
+/* reenable interactivity of calling thread */
+void unlock(){
+	sigset_t unblocked_sig;
+	sigemptyset(&unblocked_sig);
+	sigaddset(&unblocked_sig, SIGALRM);
+
+	sigprocmask(SIG_UNBLOCK, &unblocked_sig, NULL);
 }
 
 /* write p to r12 */
@@ -130,4 +213,77 @@ void wr_reg12(unsigned long int p){
 /* write p to r13 */
 void wr_reg13(unsigned long int p){
 	asm("movq %0, %%r13;": : "r"(p));
+}
+
+/* semaphore functions  */
+
+int sem_init(sem_t *sem, int pshared, unsigned value){
+	lock();
+	sema* initSem = malloc(sizeof(sema));
+	initSem -> val = value;
+	initSem -> init = 1;
+	initSem -> head = NULL;
+	sem -> __align = (long int)initSem;
+	unlock();
+	return 0;
+}
+
+int sem_wait(sem_t *sem){
+	lock();
+	sema* checkSem = (sema*)(sem -> __align);
+	if(checkSem -> val == 0){
+		if(checkSem -> head == NULL){
+			checkSem -> head = malloc(sizeof(tq));
+			checkSem -> head -> next = NULL;
+			checkSem -> head -> t = currThread;
+		} else {
+			tq* step = checkSem -> head;
+			while(step -> next != NULL){
+				step = step -> next;
+			}
+			step -> next = malloc(sizeof(tq));
+			step -> next -> next = NULL;
+			step -> next -> t = currThread;
+		}
+		currThread -> state = PAUSED;
+		unlock();
+		schedule();
+	}
+	--(checkSem -> val);
+	unlock();
+	return 0;
+}
+
+int sem_post(sem_t *sem){
+	lock();
+	sema* checkSem = (sema*)(sem -> __align);
+	
+	if(checkSem -> head != NULL){
+		checkSem -> head -> t -> state = READY;
+		tq *temp = checkSem -> head;
+		checkSem -> head = checkSem -> head -> next;
+		free(temp);
+	}
+
+	++(checkSem -> val);
+	unlock();
+	return 0;
+}
+
+int sem_destroy(sem_t *sem){
+	lock();
+	sema* deleteSum = (sema*)(sem -> __align);
+	if(deleteSum -> head != NULL){
+		tq* step = deleteSum -> head;
+		while(step -> next != NULL){
+			tq* temp = step -> next;
+			free(step);
+			step = temp;
+		}
+		free(step);
+	}
+
+	free(deleteSum);
+	unlock();
+	return 0;
 }
--- thread/threads.h
+++ thread/threads.h
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <semaphore.h>
 #include "ec440threads.h"
 
 
@@ -23,22 +24,60 @@
 #define JB_RSP  6
 #define JB_PC   7
 
+#define READY 0
+#define RUNNING 1
+#define EXIT 2
+#define PAUSED 3
+#define BLOCKED 4
+
+/* thread control block */
 typedef struct tcb{
-	pthread_t id;		// registers the thread id
-    struct tcb *next;	// tracks next thread to be executed
-    struct tcb *last;	// tracks last thread to be executed
-    jmp_buf buf;		// stores the register values
-    void *stack_ptr;	// points to threads space on the stack
-    int state;			// keeps track of thread state: 0 = READY, 1 = RUNNING, 2 = EXIT, 3 = PAUSE
+	pthread_t id;           // registers the thread id
+    struct tcb *next;       // tracks next thread to be executed
+    struct tcb *last;       // tracks last thread to be executed
+    jmp_buf buf;            // stores the register values
+    void *stack_ptr;        // points to threads space on the stack
+    int state;              // keeps track of thread state: 0 = READY, 1 = RUNNING, 2 = EXIT, 3 = PAUSED, 4 = BLOCKED
+    void* ret_val;          // stores thread return value on exit
+    pthread_t tgt_thread;    // stores the id of the blocking thread
 } nthread;
 
-/* function declarations */
+/* keeps track of the threads controlled by the semaphore */
+typedef struct thread_queue{
+    nthread* t;
+    struct thread_queue* next;
+}tq;
+
+/* stores semaphore variables and controlled threads */
+typedef struct sem_struct{
+    int init;
+    int val;
+    tq* head;
+}sema;
+
+/* threading functions */
 pthread_t pthread_self(void);
 void pthread_exit(void *value_ptr);
 int pthread_create(pthread_t *thread,const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg);
-int init(void);
+int pthread_join(pthread_t thread, void **value_ptr);
+
+/* semaphore functions */
+int sem_init(sem_t *sem, int pshared, unsigned value);
+int sem_wait(sem_t *sem);
+int sem_post(sem_t *sem);
+int sem_destroy(sem_t *sem);
+
+/* helper functions */
+int init();
 void schedule();
+void lock();
+void unlock();
+void pthread_exit_wrapper();
+nthread* thread_exists(nthread* start, pthread_t thread);
+
+/* ASM functions for editing registers */
 void wr_reg12(unsigned long int p);
 void wr_reg13(unsigned long int p);
+
 
 #endif
